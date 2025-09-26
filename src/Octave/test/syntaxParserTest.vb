8541196ad6c71f83219d5a11fa38c213fdf8c849
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-26 14:12:38 +0800

    configs syntax test

--- src/Octave/test/syntaxParserTest.vb
+++ src/Octave/test/syntaxParserTest.vb
@@ -1,6 +1,21 @@
-﻿Module syntaxParserTest
+﻿Imports Octave
+
+Module syntaxParserTest
 
     Sub run()
+        Call run("x = 3 # value 3 is assigned to symbol x")
+        Call run("x = 3 % value 3 is assigned to symbol x")
+        Call run("
+%{
+value 3 is assigned to symbol x
+%}
+x = 3")
+    End Sub
 
+    Private Sub run(script As String)
+        Dim scanner As New Scanner(script)
+        Dim tokens = scanner.GetTokens.ToArray
+
+        Pause()
     End Sub
 End Module
