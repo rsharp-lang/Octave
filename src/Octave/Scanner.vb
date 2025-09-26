19cdb81ca458aad7cd636197e37161df548a2149
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-26 13:47:05 +0800

    fix of the symbol reference error

--- src/Octave/Scanner.vb
+++ src/Octave/Scanner.vb
@@ -1,3 +1,10 @@
+Imports Microsoft.VisualBasic.Text.Parser
+Imports SMRUCC.Rsharp.Language.TokenIcer
+
 Public Class Scanner
+
+    Dim code As CharPtr
+    Dim buffer As New CharBuffer
+    Dim escape As New Escapes
 
 End Class
