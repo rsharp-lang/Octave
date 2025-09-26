fe35c226fce1cec0dcc2cbc9d17fa8b71b0a98a7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-26 14:08:53 +0800

    try to implements of the octave comment syntax

--- src/Octave/Scanner.vb
+++ src/Octave/Scanner.vb
@@ -1,4 +1,7 @@
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Text
 Imports Microsoft.VisualBasic.Text.Parser
+Imports SMRUCC.Rsharp.Language
 Imports SMRUCC.Rsharp.Language.TokenIcer
 
 Public Class Scanner
@@ -7,4 +10,68 @@ Public Class Scanner
     Dim buffer As New CharBuffer
     Dim escape As New Escapes
 
+    ''' <summary>
+    ''' 当前的代码行号
+    ''' </summary>
+    Dim lineNumber As Integer = 1
+
+
+    Sub New(source As [Variant](Of String, CharPtr))
+        If source Like GetType(String) Then
+            Me.code = source.TryCast(Of String).SolveStream
+        Else
+            Me.code = source.TryCast(Of CharPtr)
+        End If
+    End Sub
+
+    Public Overridable Iterator Function GetTokens() As IEnumerable(Of Token)
+        Dim token As New Value(Of Token)
+        Dim start As Integer = 0
+
+        Do While Not code
+            If Not (token = walkChar(++code)) Is Nothing Then
+
+            End If
+        Loop
+    End Function
+
+    Private Function walkChar(c As Char) As Token
+        If c = ASCII.LF Then
+            lineNumber += 1
+        End If
+
+        If escape.comment Then
+            If c = ASCII.CR OrElse c = ASCII.LF Then
+                If escape.isBlockComment Then
+                    buffer += c
+                Else
+                    Dim commentText As New String(buffer.PopAllChars)
+
+                    Return New Token With {
+                        .name = TokenType.comment,
+                        .text = commentText
+                    }
+                End If
+            Else
+                buffer += c
+            End If
+        ElseIf c = "#"c OrElse c = "%"c AndAlso buffer = 0 Then
+            escape.comment = True
+            buffer += c
+        ElseIf c = "#"c OrElse c = "%"c Then
+            ' follow with the expression
+            Dim token As Token = getToken(Nothing)
+
+            escape.comment = True
+            buffer += c
+
+            Return token
+        End If
+
+        Return Nothing
+    End Function
+
+    Private Function getToken(Optional bufferNext As Char? = Nothing) As Token
+
+    End Function
 End Class
