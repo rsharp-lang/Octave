cde26d8cb48d50d82baa6e9608e1ed9be1325829
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-01 20:31:55 +0800

    fix of the arguments error

---
+++ src/Octave/SyntaxBuilderOptions.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.Text.Parser
+Imports SMRUCC.Rsharp.Language.Syntax.SyntaxParser
+Imports SMRUCC.Rsharp.Language.TokenIcer
+
+Public Class OctaveSyntaxBuilderOptions : Inherits SyntaxBuilderOptions
+
+    Public Overrides Function ParseExpression(tokens As IEnumerable(Of Token)) As SyntaxResult
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function NewScanner(buffer As CharPtr, stringInterpolateParser As Boolean) As IScanner
+        Return New Scanner(buffer)
+    End Function
+End Class
